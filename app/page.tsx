9030ff4d26bb6d2c159f4b4c80be1ac86db0e5ca
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-19 21:05:52 +0530

    refactor: remove unused article fetching and formatting code from page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,7 @@
 import React from "react";
-import Image from "next/image";
-
-const response = await fetch(
-  `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/articles?populate=*`,
-);
-const articles = await response.json();
-
-const formatDate = (date: Date) => {
-  const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  };
-  return new Date(date).toLocaleDateString("en-US", options);
-};
 
 const page = () => {
-  return (
-    <div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default page;
