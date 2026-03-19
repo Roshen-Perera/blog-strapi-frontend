5a87a8856fb06047be238c351f9731d9ad8c371e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-19 21:10:44 +0530

    feat: implement page component to display articles with loading state

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,17 @@ const getArticles = async () => {
 };
 
 const page = () => {
-  return <div></div>;
+  const articles = getArticles();
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold mb-8">
+        Next.js and Strapi Integration
+      </h1>
+      <Suspense fallback={<div>Loading....</div>}>
+        <Articles articles={articles} />
+      </Suspense>
+    </div>
+  );
 };
 
 export default page;
