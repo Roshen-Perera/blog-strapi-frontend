ab4db122e6768824adb2d768c783c963b2ae5ef6
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2026-03-19 17:25:10 +0530

    refactor: remove redundant comments and clean up code in page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
-// Path: ./src/app/page.tsx
-
 import Image from "next/image";
-
-// Path: ./src/app/page.tsx
 
 const response = await fetch(
   `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/articles?populate=*`,
 );
-const articles = await response.json();
-
-// Path: ./src/app/page.tsx
+const articles = await response.json();\
 
 const formatDate = (date: Date) => {
   const options: Intl.DateTimeFormatOptions = {
